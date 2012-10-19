a70c47e3761b111659f99280057b29cf31f57723
Manuel Ebert <maebert@uos.de>
2012-10-19 22:12:58 +0200

    Inits the DB

--- run.py
+++ run.py
@@ -4,7 +4,15 @@
 import sys
 import cnoms
 
+def init(app):
+    from cnoms import models
+    models.Entry.create_table(fail_silently=True)
+
 if __name__ == "__main__":
     local = "-l" in sys.argv
     app = cnoms.create_app(local)
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+
+    if "init" in sys.argv:
+        init(app)
+    else:
+        app.run(host=app.config['HOST'], port=app.config['PORT'])
